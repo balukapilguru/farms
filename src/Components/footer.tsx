e10675ac6d706a1a3517d6716e1b6692a035f0d4
Kapilgroup-kp <ajay.b@kapilgroup.com>
2024-08-26 12:38:26 +0530

    Update footer.tsx

--- src/Components/footer.tsx
+++ src/Components/footer.tsx
@@ -48,7 +48,7 @@ const Footer = () => {
         <div className="lg:flex mt-5">
           <div className="md:w-2/3 sm:flex sm:flex-col sm:items-center lg:block">
             <Image src={Qr} alt="qr" className="w-40" />
-            <p className="text-white text-md mt-2">For more info scan here<br/> Number - 9705311222</p>
+            <p className="text-white text-md mt-2">For more info scan here<br/> Number - 9839837280</p>
           </div>
           <div className="lg:w-2/3 flex flex-col justify-end py-5 sm:hidden md:flex lg:flex">
             <h1 className="text-3xl font-bold text-[#7ABF4E] tracking-wide lg:text-right ">
