4e88b2d31726d2c0dfd74039ec342dd8b854abc8
Kapilgroup-kp <ajay.b@kapilgroup.com>
2024-08-26 18:38:19 +0530

    Update header.tsx

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -74,8 +74,8 @@ const Header = () => {
         </ul>
       </nav>
       <div className="lg:flex mt-5">
-            <a href="tel:+919839837280" className="text-[16px] lg:text-green sm:text-white">Call: +91-9839837280</a><br>
-            <a href="mailto:marketing@kapilfarms.in" className="text-[16px] lg:text-green sm:text-white">Email: marketing@kapilfarms.in</a>
+            <p><a href="tel:+919839837280" className="text-[16px] lg:text-green sm:text-white">Call: +91-9839837280</a></p>
+            <p><a href="mailto:marketing@kapilfarms.in" className="text-[16px] lg:text-green sm:text-white">Email: marketing@kapilfarms.in</a></p>
          
         </div>
       </header>
