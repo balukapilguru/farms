272dd043c0335e3965a283ed04a31f43406381c1
Kapilgroup-kp <ajay.b@kapilgroup.com>
2024-08-27 14:25:16 +0530

    Update GoogleTagManager.tsx

--- src/Components/GoogleTagManager.tsx
+++ src/Components/GoogleTagManager.tsx
@@ -2,22 +2,15 @@ import Script from "next/script";
 
 const GoogleTagManager = ({ gtmId }: { gtmId: string }) => (
   <>
-    <Script
-      src={`https://www.googletagmanager.com/gtag/js?id=${gtmId}`}
-      strategy="afterInteractive"
-    />
-    <Script
-      id="google-analytics"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${gtmId}');
-        `,
-      }}
-    />
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11454847573"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11454847573');
+</script>
   </>
 );
 
