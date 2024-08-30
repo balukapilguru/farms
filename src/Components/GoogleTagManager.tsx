e4a30f9a4da1694240216ea6cf9b4477223e9a40
Kapilgroup-kp <ajay.b@kapilgroup.com>
2024-08-30 13:12:21 +0530

    Update GoogleTagManager.tsx

--- src/Components/GoogleTagManager.tsx
+++ src/Components/GoogleTagManager.tsx
@@ -7,7 +7,7 @@ const GoogleTagManager = ({ gtmId }: { gtmId: string }) => (
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P28GH3CZ');</script>
+})(window,document,'script','dataLayer','GTM-P28GH3CZ'); </script>
   </>
 );
 
