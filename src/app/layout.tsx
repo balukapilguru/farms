27702301dc063f30662be179bd2a71de72837d71
Kapilgroup-kp <ajay.b@kapilgroup.com>
2024-08-30 14:26:18 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,14 @@ export default function RootLayout({
           {children}
           <Footer />
         </Container>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P28GH3CZ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P28GH3CZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
